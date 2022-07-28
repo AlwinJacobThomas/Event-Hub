a22698977545b6cbcb0cb2fc7a99cb448177d563
vishDroid-dev <kailath.ajt@gmail.com>
2022-07-28 15:41:36 +0530

    made- 2html pages|models.py club and event(not migrated)|added static and media dirs(core/uploads)

--- core/event/models.py
+++ core/event/models.py
@@ -1,3 +1,31 @@
+from distutils.command.upload import upload
+import email
+from operator import mod
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class event(models.Model):
+    e_name = models.CharField(max_length=50)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    e_date = models.DateField(auto_now_add=True)
+    desc = models.TextField()
+    p_limit = models.IntegerField()
+    brochure = models.ImageField()
+    #noti = models.ForeignKey(Notification,on_delete=models.CASCADE)
+    #host = models.ForeignKey(Club,on_delete=models.CASCADE)
+    #participants = models.ForeignKey(Student,on_delete=models.CASCADE)
+    Venue = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.e_name
+
+class Club(models.Model):
+    c_name = models.CharField(max_length=255)
+    c_about = models.TextField()
+    profile_pic = models.ImageField(upload_to='club')
+    #c_admin = models.ForeignKey(Student,on_delete=models.CASCADE)
+    
+
+
+
--- core/event/views.py
+++ core/event/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
+from unicodedata import name
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    events=[
+        {
+            'title':"paradox",
+            'club':"IEEE",
+            'type':"Tech Fest"
+        },
+        {
+            'title':"Lumere",
+            'club':"UNION",
+            'type':"National Tech and Cultural Fest"
+        },
+        {
+            'title':"Confictura",
+            'club':"Ajagajans",
+            'type':"department fest"
+        }
+    ]
+    return render(request,'index.html',{"events":events})
