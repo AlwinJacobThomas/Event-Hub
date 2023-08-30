fd5e951e0f2cc4e948e5519091161ae34328aa09
Alwin J Thomas <33421258+AlwinJacobThomas@users.noreply.github.com>
2023-08-30 12:46:41 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-)pi+rd6e3$k%c(p&3e3-%$!y!@n#1+$@1gbzmi!9mnik0zh$pr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['event-hub-cek.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['event-ayjguwdki-alwinjacobthomas.vercel.app']
 
 
 # Application definition
